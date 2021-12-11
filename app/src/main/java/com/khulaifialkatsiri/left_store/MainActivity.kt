b7a6a7cb50af6e6e914192f6d78e0ecc4b75cee6
leffial <leffial@gmail.com>
2021-12-11 21:59:39 +0700

    update xml

--- app/src/main/java/com/khulaifialkatsiri/left_store/MainActivity.kt
+++ app/src/main/java/com/khulaifialkatsiri/left_store/MainActivity.kt
@@ -2,10 +2,32 @@ package com.khulaifialkatsiri.left_store
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+    private var webView: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        webView = findViewById(R.id.wb_webView)
+        webView!!.webViewClient = WebViewClient()
+        webView!!.loadUrl("https://sepatu.left-store.biz/")
+
+        val webSettings = webView!!.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()){
+            webView!!.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
